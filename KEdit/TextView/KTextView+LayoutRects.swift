d98211099594a6e70aec7c547be8b0b29b05bd8d
krnsoftware <mkarino@gmai.com>
2025-06-26 22:18:23 +0900

    空行をクリックしてもその行が選択されない不具合を解決。

--- KEdit/TextView/KTextView+LayoutRects.swift
+++ KEdit/TextView/KTextView+LayoutRects.swift
@@ -23,13 +23,13 @@ struct LayoutRects {
         static let `default` = TextEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
     }
     
-    private let bounds: CGRect
-    private let visibleRect: CGRect
-    private let layoutManagerRef: KLayoutManagerReadable
-    private let textStorageRef: KTextStorageReadable
+    private let _bounds: CGRect
+    private let _visibleRect: CGRect
+    private let _layoutManagerRef: KLayoutManagerReadable
+    private let _textStorageRef: KTextStorageReadable
     
     // LineNumberRegionの行数表示の最小桁数
-    private let minimumLineNumberCharacterWidth = 3
+    private let _minimumLineNumberCharacterWidth = 3
     //private let lineNumberWidth: CGFloat
     
     let textRegion: Region
@@ -47,18 +47,15 @@ struct LayoutRects {
     }
     
     
-    init(layoutManagerRef: KLayoutManagerReadable, textStorageRef: KTextStorageReadable, bounds: CGRect, visibleRect: CGRect, /*lineNumberWidth: CGFloat, */showLineNumbers: Bool, textEdgeInsets: TextEdgeInsets = .default) {
-        self.bounds = bounds
-        self.visibleRect = visibleRect
-        //self.padding = padding
+    init(layoutManagerRef: KLayoutManagerReadable, textStorageRef: KTextStorageReadable, bounds: CGRect, visibleRect: CGRect, showLineNumbers: Bool, textEdgeInsets: TextEdgeInsets = .default) {
+        _bounds = bounds
+        _visibleRect = visibleRect
+        _layoutManagerRef = layoutManagerRef
+        _textStorageRef = textStorageRef
         self.showLineNumbers = showLineNumbers
-        self.layoutManagerRef = layoutManagerRef
-        self.textStorageRef = textStorageRef
         self.textEdgeInsets = textEdgeInsets
-        
-        //print("layoutrects, init: bounds: \(bounds), lineNumberWidth: \(lineNumberWidth), padding: \(padding), showLineNumbers: \(showLineNumbers)")
-        
-        let digitCount = max(minimumLineNumberCharacterWidth, "\(layoutManagerRef.lineCount)".count)
+                
+        let digitCount = max(_minimumLineNumberCharacterWidth, "\(layoutManagerRef.lineCount)".count)
         let attrStr = NSAttributedString(string: "M", attributes: [.font: NSFont.monospacedDigitSystemFont(ofSize: textStorageRef.baseFont.pointSize * 0.95, weight: .regular)])
         let ctLine = CTLineCreateWithAttributedString(attrStr)
         let charWidth = CGFloat(CTLineGetTypographicBounds(ctLine, nil, nil, nil))
@@ -75,10 +72,6 @@ struct LayoutRects {
         let textWidth = CGFloat(layoutManagerRef.maxLineWidth) + textEdgeInsets.left + lineNumberWidth + textEdgeInsets.right
         let textHeight = CGFloat(layoutManagerRef.lineCount) * layoutManagerRef.lineHeight + textEdgeInsets.top + textEdgeInsets.bottom + visibleRect.height * 0.67 // 見えている領域の2/3くらいの高さを余分に設定する。
        
-        /*let textRect = CGRect(x: 0,
-                              y: 0,
-                              width: CGFloat(layoutManagerRef.maxLineWidth) + textEdgeInsets.left + lineNumberWidth + textEdgeInsets.right,
-                              height: CGFloat(layoutManagerRef.lineCount) * layoutManagerRef.lineHeight + textEdgeInsets.top + textEdgeInsets.bottom)*/
         let textRect = CGRect(x: 0, y: 0, width: textWidth, height: textHeight)
 
         self.textRegion = Region(rect: textRect)
@@ -98,18 +91,16 @@ struct LayoutRects {
         let lines = layoutManager.lines
         let lineCount = lines.count
 
+        // LineNumberRegionの場合
         if let lnRect = lineNumberRegion?.rect, lnRect.contains(point) {
             let lineIndex = min(Int(point.y / lineHeight), lineCount - 1)
             print("regionType - in lineNumberRegion: \(lineIndex)")
             return .lineNumber(line: lineIndex)
         }
 
+        // TextRegionの場合
         if textRegion.rect.contains(point) {
             
-            /*let relativePoint = CGPoint(
-                x: point.x - textRegion.rect.origin.x,
-                y: point.y - textRegion.rect.origin.y
-            )*/
             let relativePoint = CGPoint(
                 x: point.x - textRegion.rect.origin.x - horizontalInsets,
                 y: point.y - textRegion.rect.origin.y - textEdgeInsets.top
@@ -123,15 +114,13 @@ struct LayoutRects {
             }
 
             let line = lines[lineIndex]
-            //let attrString = NSAttributedString(string: line.text, attributes: [.font: textStorage.baseFont])
-            //let ctLine = CTLineCreateWithAttributedString(attrString)
             let ctLine = line.ctLine
             let relativeX = max(0, relativePoint.x)
             let indexInLine = CTLineGetStringIndexForPosition(ctLine, CGPoint(x: relativeX, y: 0))
             
-            //print("regionType - in textRegion, lineIndex=\(lineIndex), indexInLine=\(indexInLine)")
+            print("regionType - in textRegion, lineIndex=\(lineIndex), indexInLine=\(indexInLine)")
             
-            return .text(index: line.range.lowerBound + indexInLine)
+            return .text(index: line.range.lowerBound + (indexInLine >= 0 ? indexInLine : 0))
         }
 
         return .outside
