1eccc115be1cb68f8d23a40f616e60aaca54d187
krnsoftware <mkarino@gmai.com>
2026-02-23 11:40:26 +0900

    画面分割している状態で一方を編集すると、編集範囲が重複していなくてももう一方の選択範囲が変更されてしまう不具合を修正。

--- Ganpi/TextView/View/KTextView.swift
+++ Ganpi/TextView/View/KTextView.swift
@@ -100,6 +100,9 @@ final class KTextView: NSView, NSTextInputClient, NSDraggingSource {
         // _liveResizeFreezeThreshold文字以上のときだけ live resize 中のレイアウト更新を止める
         inLiveResize && (_textStorageRef.count >= _liveResizeFreezeThreshold)
     }
+    
+    // textStorageを編集しているのが自分であるか否かのフラグ
+    private var _isSelfEditing = false
 
     
     // Text Input Clientの実装。
@@ -1704,6 +1707,8 @@ final class KTextView: NSView, NSTextInputClient, NSDraggingSource {
         }
 
         let string = rawString.normalizedString
+        _isSelfEditing = true
+        defer { _isSelfEditing = false }
         textStorage.replaceCharacters(in: range, with: Array(string))
 
         _markedTextRange = nil
@@ -1875,7 +1880,7 @@ final class KTextView: NSView, NSTextInputClient, NSDraggingSource {
         case let .textChanged(info):
             let delta = info.insertedCount - info.range.count
             
-            if info.range.lowerBound == selectionRange.lowerBound /*(削除+)追記*/ ||
+            if _isSelfEditing, info.range.lowerBound == selectionRange.lowerBound /*(削除+)追記*/ ||
                 info.range.upperBound == selectionRange.lowerBound /*1文字削除*/ {
                 // このtextviewによる編集。
                 caretIndex = info.range.lowerBound + info.insertedCount
